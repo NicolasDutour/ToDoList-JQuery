61b897928939d27a880259ae3ac52dd2f70f8058
NicolasDutour <dutourn99@gmail.com>
2018-03-09 17:10:34 +0100

    Add all files

---
+++ index.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  $('.add-items').submit(function(event) {
+    event.preventDefault();
+
+    var item = $('.todo-list-item').val();
+
+    if (item) {
+      $('.list-items').append("<li><input class='checkbox' type='checkbox' />" + item + "<a class='remove'>x</a><hr></li>");
+      $('.todo-list-item').val("");
+    }
+
+    $(document).on('change', '.checkbox',function() {
+      $(this).parent().toggleClass('completed');
+    });
+
+    $(document).on('click', '.remove',function() {
+      $(this).parent().remove();
+    });
+
+  });
+});
